c8a19c19770cc3707e3cb44e2663731f8c1c16a1
đặng việt hùng <dangviethung213@gmail.com>
2025-03-09 16:37:30 +0700

    no1

---
+++ app/src/main/java/com/example/appxemphim/object_data/Move.java
@@ -0,0 +1,79 @@
+package com.example.appxemphim.object_data;
+
+import java.util.Date;
+
+public class Move {
+    private int movive_Id;
+    private String title; //tên của bộ phim
+    private String description; // mô tả của bộ phim
+    private String poster_url; // link ảnh poster
+    private String trailer_url;// link trailer của bộ phim
+    private String nation; // bộ phim thuộc quốc gia nào
+    private Date created_at; // thời gian tạo phim
+
+    public Move(int movive_Id, String title, String description, String poster_url, String trailer_url, String nation, Date created_at) {
+        this.movive_Id = movive_Id;
+        this.title = title;
+        this.description = description;
+        this.poster_url = poster_url;
+        this.trailer_url = trailer_url;
+        this.nation = nation;
+        this.created_at = created_at;
+    }
+
+    public int getMovive_Id() {
+        return movive_Id;
+    }
+
+    public void setMovive_Id(int movive_Id) {
+        this.movive_Id = movive_Id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getPoster_url() {
+        return poster_url;
+    }
+
+    public void setPoster_url(String poster_url) {
+        this.poster_url = poster_url;
+    }
+
+    public String getTrailer_url() {
+        return trailer_url;
+    }
+
+    public void setTrailer_url(String trailer_url) {
+        this.trailer_url = trailer_url;
+    }
+
+    public String getNation() {
+        return nation;
+    }
+
+    public void setNation(String nation) {
+        this.nation = nation;
+    }
+
+    public Date getCreated_at() {
+        return created_at;
+    }
+
+    public void setCreated_at(Date created_at) {
+        this.created_at = created_at;
+    }
+}
---
+++ app/src/main/java/com/example/appxemphim/object_data/Video.java
@@ -0,0 +1,5 @@
+package com.example.appxemphim.object_data;
+
+public class Video {
+
+}
