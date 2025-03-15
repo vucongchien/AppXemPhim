896c49a9f3296fb82fa5976e847e328d91a0df03
đặng việt hùng <dangviethung213@gmail.com>
2025-03-15 15:29:28 +0700

    addmovie

--- app/src/main/java/com/example/appxemphim/object_data/Video.java
+++ app/src/main/java/com/example/appxemphim/object_data/Video.java
@@ -1,35 +1,62 @@
 package com.example.appxemphim.object_data;
 
 import java.sql.Time;
+import java.util.HashMap;
+
+import android.media.MediaMetadataRetriever;
+import java.io.IOException;
 
 public class Video {
-    private int video_id;
+    private String video_id;
+
     private String video_url;
     private Time duration;
-    private int movie_id;
     private int view;
 
-    public Video(int video_id, String video_url, Time duration, int movie_id, int view) {
-        this.video_id = video_id;
+    public Video( String video_url, Time duration, int movie_id, int view) {
         this.video_url = video_url;
         this.duration = duration;
-        this.movie_id = movie_id;
         this.view = view;
+    }
+
+    public Video(String video_url) {
+        this.video_url = video_url;
+    }
+
+    public Video(String video_id, String video_url, int view, Time duration) {
+        this.video_id = video_id;
+        this.video_url = video_url;
+        this.view = view;
+        this.duration = duration;
     }
 
     public Time getDuration() {
         return duration;
     }
 
-    public void setDuration(Time duration) {
-        this.duration = duration;
+    public void setDuration(String videoUrl) throws IOException {
+        MediaMetadataRetriever retriever = new MediaMetadataRetriever();
+        try {
+            retriever.setDataSource(videoUrl, new HashMap<>());
+            String time = retriever.extractMetadata(MediaMetadataRetriever.METADATA_KEY_DURATION);
+
+            if (time != null) {
+                duration = new Time(Long.parseLong(time));
+            } else {
+                duration = new Time(0);
+            }
+        } catch (Exception e) {
+            duration = new Time(0);
+        } finally {
+            retriever.release();
+        }
     }
 
-    public int getVideo_id() {
+    public String getVideo_id() {
         return video_id;
     }
 
-    public void setVideo_id(int video_id) {
+    public void setVideo_id(String video_id) {
         this.video_id = video_id;
     }
 
@@ -41,13 +68,6 @@ public class Video {
         this.video_url = video_url;
     }
 
-    public int getMovie_id() {
-        return movie_id;
-    }
-
-    public void setMovie_id(int movie_id) {
-        this.movie_id = movie_id;
-    }
 
     public int getView() {
         return view;
