0a3afc80449ddf07595ef31ea1f41efb380d274a
đặng việt hùng <dangviethung213@gmail.com>
2025-03-15 20:49:58 +0700

    loi lui

--- app/src/main/java/com/example/appxemphim/FireStore_DataBase/Add_Data.java
+++ app/src/main/java/com/example/appxemphim/FireStore_DataBase/Add_Data.java
@@ -5,12 +5,15 @@ import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 
+import com.example.appxemphim.Utilities.GoogleDriveUtils;
 import com.example.appxemphim.object_data.Movie;
 import com.example.appxemphim.object_data.Video;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.firestore.FieldValue;
 import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.io.IOException;
 
 public class Add_Data {
     public static void Add_Data_movie(Context context,FirebaseFirestore db, Movie movie){
@@ -27,17 +30,32 @@ public class Add_Data {
                     );
         }
     }
-    public static  void  Add_Data_video(Context context, FirebaseFirestore db, Video video, String maphim){
+    public static  void  Add_Data_video(Context context, FirebaseFirestore db, Video video, String maphim) {
         String videoId = db.collection("Video").document().getId();
         if(videoId!=null){
-            db.collection("movies").document(maphim)
-                    .update("videos", FieldValue.arrayUnion(video))
-                    .addOnSuccessListener(aVoid ->
-                            Toast.makeText(context, "Video đã được thêm vào movie!", Toast.LENGTH_SHORT).show()
-                    )
-                    .addOnFailureListener(e ->
-                            Toast.makeText(context, "Lỗi khi thêm video: " + e.getMessage(), Toast.LENGTH_LONG).show()
-                    );
+            video.setVideo_id(videoId);
+            GoogleDriveUtils.getVideoDuration(context, video.getVideo_url(), new GoogleDriveUtils.VideoDurationCallback() {
+                @Override
+                public void onSuccess(long durationMillis) {
+                    video.setDuration(durationMillis);
+                    Toast.makeText(context, "Thời gian video: " + video.getDuration() + " ms", Toast.LENGTH_SHORT).show();
+                    db.collection("movies").document(maphim)
+                            .update("videos", FieldValue.arrayUnion(video))
+                            .addOnSuccessListener(aVoid ->
+                                    Toast.makeText(context, "Video đã được thêm vào movie!", Toast.LENGTH_SHORT).show()
+                            )
+                            .addOnFailureListener(e ->
+                                    Toast.makeText(context, "Lỗi khi thêm video: " + e.getMessage(), Toast.LENGTH_LONG).show()
+                            );
+                }
+
+                @Override
+                public void onError(String errorMessage) {
+                    Toast.makeText(context, errorMessage, Toast.LENGTH_SHORT).show();
+                }
+            });
+
+
         }
     }
 
--- app/src/main/java/com/example/appxemphim/object_data/Video.java
+++ app/src/main/java/com/example/appxemphim/object_data/Video.java
@@ -1,24 +1,32 @@
 package com.example.appxemphim.object_data;
 
+import static com.example.appxemphim.Utilities.GoogleDriveUtils.getVideoDuration;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.sql.Time;
 import java.util.HashMap;
 
+import android.content.Context;
 import android.media.MediaMetadataRetriever;
+import android.widget.Toast;
+
+
+import com.example.appxemphim.Utilities.GoogleDriveUtils;
+
 import java.io.IOException;
 
 public class Video {
     private String video_id;
     private String video_url;
-    private Time duration;
+    private long duration;
     private int view;
 
-    public Video(String video_url, Time duration, int view) {
-        this.video_url = video_url;
-        this.duration = duration;
-        this.view = 0;
-    }
 
-    public Video(String video_id, String video_url, Time duration, String movie_id, int view) {
+    public Video(String video_id, String video_url, long duration, int view) {
         this.video_id = video_id;
         this.video_url = video_url;
         this.duration = duration;
@@ -29,34 +37,16 @@ public class Video {
         this.video_url = video_url;
     }
 
-    public Video(String video_id, String video_url, int view, Time duration) {
-        this.video_id = video_id;
-        this.video_url = video_url;
-        this.view = view;
-        this.duration = duration;
-    }
 
-    public Time getDuration() {
+    public long getDuration() {
         return duration;
     }
 
-    public void setDuration(String videoUrl) throws IOException {
-        MediaMetadataRetriever retriever = new MediaMetadataRetriever();
-        try {
-            retriever.setDataSource(videoUrl, new HashMap<>());
-            String time = retriever.extractMetadata(MediaMetadataRetriever.METADATA_KEY_DURATION);
 
-            if (time != null) {
-                duration = new Time(Long.parseLong(time));
-            } else {
-                duration = new Time(0);
-            }
-        } catch (Exception e) {
-            duration = new Time(0);
-        } finally {
-            retriever.release();
-        }
+    public void setDuration(long duration) {
+        this.duration=duration;
     }
+
 
     public String getVideo_id() {
         return video_id;
