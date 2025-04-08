0d19ea75909d6787ada0630439a1b52a44fa820b
tungpy78 <truonganhtung230904@gmail.com>
2025-04-08 16:38:46 +0700

    login register verify reset

---
+++ app/src/main/java/com/example/appxemphim/ForgotPassword.java
@@ -0,0 +1,51 @@
+package com.example.appxemphim;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class ForgotPassword extends AppCompatActivity {
+    EditText editTextEmail;
+    Button btnSendEmail;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_forgot_password);
+
+        editTextEmail = findViewById(R.id.editTextEmailtoOtp);
+        btnSendEmail = findViewById(R.id.buttonSendEmail);
+
+        btnSendEmail.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String email = editTextEmail.getText().toString();
+                if(email.equals("truongtung9ctcv@gmail.com")){
+                    startActivity(new Intent(ForgotPassword.this, VerifyOTP.class));
+                }else{
+                    Toast.makeText(ForgotPassword.this, "email không tồn tại", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+
+    }
+
+    public void BackSign(View view) {
+        startActivity(new Intent(ForgotPassword.this, Login.class));
+    }
+
+    public void Back(View view) {
+        finish();
+    }
+}
--- app/src/main/java/com/example/appxemphim/Login.java
+++ app/src/main/java/com/example/appxemphim/Login.java
@@ -3,6 +3,9 @@ package com.example.appxemphim;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -11,16 +14,30 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class Login extends AppCompatActivity {
+    EditText editTextUserName, editTextPassword;
+    Button btnLogin;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_login);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+        editTextUserName = findViewById(R.id.editTextUserName);
+        editTextPassword = findViewById(R.id.editTextPassword);
+        btnLogin = findViewById(R.id.buttonLogin);
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String username = editTextUserName.getText().toString();
+                String password = editTextPassword.getText().toString();
+                if(username.equals("admin") && password.equals("tung12345")){
+                    Toast.makeText(Login.this, "Đăng nhập thành công", Toast.LENGTH_SHORT).show();
+                }else{
+                    Toast.makeText(Login.this, "Sai tài khoản hoặc mật khẩu", Toast.LENGTH_SHORT).show();
+                }
+            }
         });
     }
 
@@ -29,8 +46,10 @@ public class Login extends AppCompatActivity {
     }
 
     public void Forgetpw(View view) {
+        startActivity(new Intent(Login.this, ForgotPassword.class));
     }
 
     public void SignGG(View view) {
     }
+
 }
