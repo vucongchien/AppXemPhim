3dbece6f2e1f49883f2e0e5344ad0a1e307c3f19
tungpy78 <truonganhtung230904@gmail.com>
2025-04-08 16:38:46 +0700

    feat: implement login, register, OTP verification and reset password

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,7 +10,7 @@ android {
     defaultConfig {
         applicationId = "com.example.appxemphim"
         minSdk = 24
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
@@ -52,8 +52,12 @@ dependencies {
     implementation("androidx.credentials:credentials:1.3.0")
     implementation("androidx.credentials:credentials-play-services-auth:1.3.0")
     implementation("com.google.android.libraries.identity.googleid:googleid:1.1.1")
+
     implementation ("com.google.android.gms:play-services-auth:20.7.0")
     implementation ("com.google.firebase:firebase-auth:22.1.1")
     implementation ("com.google.android.material:material:1.6.0")
+
+
+
 
 }
