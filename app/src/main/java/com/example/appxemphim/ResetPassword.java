2e97cb18d6ebc40b9a503fd6395457547ca80cfb
đặng việt hùng <dangviethung213@gmail.com>
2025-04-17 10:05:17 +0700

    feat: optimize reset password workflow

--- app/src/main/java/com/example/appxemphim/ResetPassword.java
+++ app/src/main/java/com/example/appxemphim/ResetPassword.java
@@ -2,6 +2,7 @@ package com.example.appxemphim;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -13,9 +14,17 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.EmailAuthProvider;
+import com.google.firebase.auth.FirebaseUser;
+
 public class ResetPassword extends AppCompatActivity {
     EditText editTextNewPassword, editTextConfirmNewPassword;
     Button buttonUpdate;
+
+    String email,otp;
+
+    //email,DTO lấy tên SharedPreferences
 
 
     @Override
@@ -37,8 +46,28 @@ public class ResetPassword extends AppCompatActivity {
                     Toast.makeText(ResetPassword.this, "Vui lòng nhập mật khẩu", Toast.LENGTH_SHORT).show();
                 }else {
                     if(newPassword.equals(confirmNewPassword)){
-                        Toast.makeText(ResetPassword.this, "Đổi mật khẩu thành công", Toast.LENGTH_SHORT).show();
-                        startActivity(new Intent(ResetPassword.this, Login.class));
+                        AuthCredential credential = EmailAuthProvider.getCredential(email, otp);
+                        MainActivity.mAuth.signInWithCredential(credential)
+                                .addOnCompleteListener(task -> {
+                                    if (task.isSuccessful()) {
+                                        MainActivity.user = MainActivity.mAuth.getCurrentUser();
+                                        if (MainActivity.user != null) {
+                                            MainActivity.user.updatePassword(newPassword)
+                                                    .addOnCompleteListener(updateTask -> {
+                                                        if (updateTask.isSuccessful()) {
+                                                            Intent intent= new Intent(ResetPassword.this, Login.class);
+                                                            intent.putExtra("gmail",email);
+                                                            intent.putExtra("pass",newPassword);
+                                                            startActivity(intent);
+                                                        } else {
+                                                            Log.e("UpdatePassword", "Cập nhật thất bại!", updateTask.getException());
+                                                        }
+                                                    });
+                                        }
+                                    } else {
+                                        Log.e("Login", "Đăng nhập thất bại!", task.getException());
+                                    }
+                                });
                     }else {
                         Toast.makeText(ResetPassword.this, "Mật khẩu không khớp", Toast.LENGTH_SHORT).show();
                     }
