d46510e03ac6e43d9b6cbe1dd9686bd2b04f51c6
đặng việt hùng <dangviethung213@gmail.com>
2025-04-19 20:28:50 +0700

    feat: implement movie details UI components

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -64,7 +64,6 @@ dependencies {
     implementation("androidx.credentials:credentials:1.3.0")
     implementation("androidx.credentials:credentials-play-services-auth:1.3.0")
     implementation("com.google.android.libraries.identity.googleid:googleid:1.1.1")
-
     implementation ("com.google.firebase:firebase-auth:22.1.1")
     implementation ("com.github.bumptech.glide:glide:4.16.0")
     implementation ("com.squareup.okhttp3:okhttp:4.9.3")
@@ -78,6 +77,10 @@ dependencies {
     implementation ("com.squareup.okhttp3:okhttp:4.12.0")
     implementation ("com.google.android.material:material:1.6.0")
 
+    implementation ("androidx.media3:media3-exoplayer:1.2.0")
+    implementation ("androidx.media3:media3-ui:1.2.0")
+    implementation ("com.github.bumptech.glide:glide:4.16.0")
+    annotationProcessor ("com.github.bumptech.glide:compiler:4.16.0")
 
 
 
--- app/src/main/java/com/example/appxemphim/Network/RetrofitClient.java
+++ app/src/main/java/com/example/appxemphim/Network/RetrofitClient.java
@@ -1,16 +1,27 @@
 package com.example.appxemphim.Network;
 
+import java.util.concurrent.TimeUnit;
+
+import okhttp3.OkHttpClient;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
 public class RetrofitClient {
-    private static final String BASE_URL = "http://192.168.100.7:8081/";
+    private static final String BASE_URL = "http://10.0.2.2:8081/";
     private static RetrofitClient instance;
     private final Retrofit retrofit;
 
     private RetrofitClient() {
+        // Cấu hình OkHttpClient với timeout
+        OkHttpClient okHttpClient = new OkHttpClient.Builder()
+                .connectTimeout(30, TimeUnit.SECONDS)  // timeout khi kết nối
+                .readTimeout(30, TimeUnit.SECONDS)     // timeout khi đọc dữ liệu
+                .writeTimeout(30, TimeUnit.SECONDS)    // timeout khi ghi dữ liệu
+                .build();
+
         retrofit = new Retrofit.Builder()
                 .baseUrl(BASE_URL)
+                .client(okHttpClient) // Gán client với timeout vào Retrofit
                 .addConverterFactory(GsonConverterFactory.create())
                 .build();
     }
