a3bb2f41c365b2c5a6afdc6d2a01291355ff9aa5
Vucongchien <vucongchien204@gmail.com>
2025-04-19 21:56:24 +0700

    lombok như cac

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -51,8 +51,6 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
-    compileOnly("org.projectlombok:lombok:1.18.30")
-    annotationProcessor("org.projectlombok:lombok:1.18.30")
     implementation(platform("com.google.firebase:firebase-bom:33.10.0"))
     implementation("com.google.android.gms:play-services-auth:21.3.0")
     implementation("com.google.firebase:firebase-analytics")
--- app/src/main/java/com/example/appxemphim/Model/MovieDetailModel.java
+++ app/src/main/java/com/example/appxemphim/Model/MovieDetailModel.java
@@ -4,14 +4,6 @@ import com.google.firebase.Timestamp;
 
 import java.util.List;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-
-
-@Data
-@AllArgsConstructor
-@NoArgsConstructor
 public class MovieDetailModel {
     private String movie_Id;
     private String title;
@@ -26,4 +18,149 @@ public class MovieDetailModel {
     private List<DirectorModel> directors;
     private List<ReviewModel> reviews;
     private List<GenreModel> genres;
+
+    public MovieDetailModel() {
+    }
+
+    public MovieDetailModel(String movie_Id, String title, String description, String poster_url,
+                          String trailer_url, double rating, String nation, Timestamp created_at,
+                          List<VideoModel> videos, List<ActorModel> actors, List<DirectorModel> directors,
+                          List<ReviewModel> reviews, List<GenreModel> genres) {
+        this.movie_Id = movie_Id;
+        this.title = title;
+        this.description = description;
+        this.poster_url = poster_url;
+        this.trailer_url = trailer_url;
+        this.rating = rating;
+        this.nation = nation;
+        this.created_at = created_at;
+        this.videos = videos;
+        this.actors = actors;
+        this.directors = directors;
+        this.reviews = reviews;
+        this.genres = genres;
+    }
+
+    public String getMovie_Id() {
+        return movie_Id;
+    }
+
+    public void setMovie_Id(String movie_Id) {
+        this.movie_Id = movie_Id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getPoster_url() {
+        return poster_url;
+    }
+
+    public void setPoster_url(String poster_url) {
+        this.poster_url = poster_url;
+    }
+
+    public String getTrailer_url() {
+        return trailer_url;
+    }
+
+    public void setTrailer_url(String trailer_url) {
+        this.trailer_url = trailer_url;
+    }
+
+    public double getRating() {
+        return rating;
+    }
+
+    public void setRating(double rating) {
+        this.rating = rating;
+    }
+
+    public String getNation() {
+        return nation;
+    }
+
+    public void setNation(String nation) {
+        this.nation = nation;
+    }
+
+    public Timestamp getCreated_at() {
+        return created_at;
+    }
+
+    public void setCreated_at(Timestamp created_at) {
+        this.created_at = created_at;
+    }
+
+    public List<VideoModel> getVideos() {
+        return videos;
+    }
+
+    public void setVideos(List<VideoModel> videos) {
+        this.videos = videos;
+    }
+
+    public List<ActorModel> getActors() {
+        return actors;
+    }
+
+    public void setActors(List<ActorModel> actors) {
+        this.actors = actors;
+    }
+
+    public List<DirectorModel> getDirectors() {
+        return directors;
+    }
+
+    public void setDirectors(List<DirectorModel> directors) {
+        this.directors = directors;
+    }
+
+    public List<ReviewModel> getReviews() {
+        return reviews;
+    }
+
+    public void setReviews(List<ReviewModel> reviews) {
+        this.reviews = reviews;
+    }
+
+    public List<GenreModel> getGenres() {
+        return genres;
+    }
+
+    public void setGenres(List<GenreModel> genres) {
+        this.genres = genres;
+    }
+
+    @Override
+    public String toString() {
+        return "MovieDetailModel{" +
+                "movie_Id='" + movie_Id + '\'' +
+                ", title='" + title + '\'' +
+                ", description='" + description + '\'' +
+                ", poster_url='" + poster_url + '\'' +
+                ", trailer_url='" + trailer_url + '\'' +
+                ", rating=" + rating +
+                ", nation='" + nation + '\'' +
+                ", created_at=" + created_at +
+                ", videos=" + videos +
+                ", actors=" + actors +
+                ", directors=" + directors +
+                ", reviews=" + reviews +
+                ", genres=" + genres +
+                '}';
+    }
 }
