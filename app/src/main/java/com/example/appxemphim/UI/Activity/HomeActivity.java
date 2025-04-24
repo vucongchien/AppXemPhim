bb89b50205ff57794d727c4a8f3978398f961db9
Vucongchien <vucongchien204@gmail.com>
2025-04-24 23:33:05 +0700

    xong home fragment

--- app/src/main/java/com/example/appxemphim/UI/Activity/HomeActivity.java
+++ app/src/main/java/com/example/appxemphim/UI/Activity/HomeActivity.java
@@ -1,24 +1,24 @@
-package com.example.appxemphim.Home;
+package com.example.appxemphim.UI.Activity;
 
-import android.annotation.SuppressLint;
 import android.os.Bundle;
 
-import androidx.activity.EdgeToEdge;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 import androidx.viewpager2.widget.ViewPager2;
 
+import com.example.appxemphim.UI.Adapter.PopularAdapter;
+import com.example.appxemphim.UI.Adapter.ViewpagerAdapter;
 import com.example.appxemphim.R;
 
 import java.util.List;
 
-public class TrangChu extends AppCompatActivity {
+public class HomeActivity extends AppCompatActivity {
     RecyclerView recyclerPopular, recyclerOnly, recyclerRetro;
     LinearLayoutManager layoutManagerPopular, layoutManagerOnly, layoutManagerRetro;
     List<Integer> popular, only, retro;
-    AdapterPopular adapterPopular, adapterRetro, adapterOnly;
+    PopularAdapter popularAdapter, adapterRetro, adapterOnly;
     ViewPager2 viewPager2;
 
     @Override
--- app/src/main/java/com/example/appxemphim/UI/Adapter/ViewpagerAdapter.java
+++ app/src/main/java/com/example/appxemphim/UI/Adapter/ViewpagerAdapter.java
@@ -1,9 +1,11 @@
-package com.example.appxemphim.Home;
+package com.example.appxemphim.UI.Adapter;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentActivity;
 import androidx.viewpager2.adapter.FragmentStateAdapter;
+
+import com.example.appxemphim.UI.Fragment.HomeFragment;
 
 public class ViewpagerAdapter extends FragmentStateAdapter {
 
