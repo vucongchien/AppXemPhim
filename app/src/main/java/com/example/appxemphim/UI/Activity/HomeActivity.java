7b35cc582201855644d97b077f1bef9b2729e8e8
Vucongchien <vucongchien204@gmail.com>
2025-04-27 12:49:25 +0700

    style: polish UI layout and theme

--- app/src/main/java/com/example/appxemphim/UI/Activity/HomeActivity.java
+++ app/src/main/java/com/example/appxemphim/UI/Activity/HomeActivity.java
@@ -1,5 +1,6 @@
 package com.example.appxemphim.UI.Activity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.annotation.Nullable;
@@ -11,24 +12,28 @@ import androidx.viewpager2.widget.ViewPager2;
 import com.example.appxemphim.UI.Adapter.PopularAdapter;
 import com.example.appxemphim.UI.Adapter.ViewpagerAdapter;
 import com.example.appxemphim.R;
+import com.example.appxemphim.databinding.ActivityHomeBinding;
+import com.example.appxemphim.databinding.ActivitySearchBinding;
 
 import java.util.List;
 
 public class HomeActivity extends AppCompatActivity {
-    RecyclerView recyclerPopular, recyclerOnly, recyclerRetro;
-    LinearLayoutManager layoutManagerPopular, layoutManagerOnly, layoutManagerRetro;
-    List<Integer> popular, only, retro;
-    PopularAdapter popularAdapter, adapterRetro, adapterOnly;
-    ViewPager2 viewPager2;
+    private ActivityHomeBinding binding;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_home);
+        binding = ActivityHomeBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
 
-        viewPager2 = findViewById(R.id.view_pager);
-        ViewpagerAdapter viewpagerAdapter = new ViewpagerAdapter(this);
-        viewPager2.setAdapter(viewpagerAdapter);
+
+        binding.viewPager.setAdapter(new ViewpagerAdapter(this));
+        binding.headerButton2.setOnClickListener(v->{
+
+            Intent intent = new Intent(HomeActivity.this, SearchActivity.class);
+            startActivity(intent);
+
+        });
 
     }
 }
