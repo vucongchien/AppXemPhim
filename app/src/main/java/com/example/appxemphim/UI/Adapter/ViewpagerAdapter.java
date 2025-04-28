a1e9f68aea91b6706f9516080c1d1a15d2106423
tungpy78 <truonganhtung230904@gmail.com>
2025-04-28 22:00:09 +0700

    tung profile

--- app/src/main/java/com/example/appxemphim/UI/Adapter/ViewpagerAdapter.java
+++ app/src/main/java/com/example/appxemphim/UI/Adapter/ViewpagerAdapter.java
@@ -2,34 +2,37 @@ package com.example.appxemphim.UI.Adapter;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentActivity;
-import androidx.viewpager2.adapter.FragmentStateAdapter;
+
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentStatePagerAdapter;
+
 
 import com.example.appxemphim.UI.Fragment.HomeFragment;
+import com.example.appxemphim.UI.Fragment.ProfileFragment;
 
-public class ViewpagerAdapter extends FragmentStateAdapter {
+public class ViewpagerAdapter extends FragmentStatePagerAdapter {
 
-    public ViewpagerAdapter(@NonNull FragmentActivity fragmentActivity) {
-        super(fragmentActivity);
+    public ViewpagerAdapter(@NonNull FragmentManager fm, int behavior) {
+        super(fm, behavior);
     }
 
     @NonNull
     @Override
-    public Fragment createFragment(int position) {
-        switch (position) {
+    public Fragment getItem(int position) {
+        switch (position){
             case 0:
-                return new HomeFragment(); // Fragment bạn đã tạo
-            // case 1:
-            //     return new AnotherFragment();
-            // case 2:
-            //     return new YetAnotherFragment();
-            default:
-                return new HomeFragment(); // fallback
+                return new HomeFragment();
+
+            case 1:
+                return new HomeFragment();
+            case 2:
+                return new ProfileFragment();
         }
+        return null;
     }
 
     @Override
-    public int getItemCount() {
-        return 1; // Số lượng fragment bạn có
+    public int getCount() {
+        return 3;
     }
 }
