81ba99038254c1ac03fa490337ee7a5fda39852d
Vucongchien <vucongchien204@gmail.com>
2025-06-04 15:02:17 +0700

    theme status bar

--- app/src/main/java/com/example/appxemphim/UI/Activity/WatchVideoActivity.java
+++ app/src/main/java/com/example/appxemphim/UI/Activity/WatchVideoActivity.java
@@ -1,11 +1,14 @@
 package com.example.appxemphim.UI.Activity;
 
 import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Looper;
 import android.util.Log;
 import android.view.View;
+import android.view.WindowInsets;
+import android.view.WindowInsetsController;
 
 import androidx.activity.EdgeToEdge;
 import androidx.annotation.NonNull;
@@ -13,6 +16,7 @@ import androidx.annotation.OptIn;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowCompat;
 import androidx.core.view.WindowInsetsCompat;
 import androidx.media3.common.MediaItem;
 import androidx.media3.common.PlaybackException;
@@ -48,9 +52,30 @@ public class WatchVideoActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         binding = ActivityWatchVideoBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
+
+        
+        WindowCompat.setDecorFitsSystemWindows(getWindow(), false);
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            final WindowInsetsController insetsController = getWindow().getInsetsController();
+            if (insetsController != null) {
+                insetsController.hide(WindowInsets.Type.statusBars() | WindowInsets.Type.navigationBars());
+                insetsController.setSystemBarsBehavior(
+                        WindowInsetsController.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE
+                );
+            }
+        } else {
+            // Dành cho Android < 11
+            getWindow().getDecorView().setSystemUiVisibility(
+                    View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+                            | View.SYSTEM_UI_FLAG_FULLSCREEN
+                            | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+            );
+        }
+
         video = (VideoModel) getIntent().getSerializableExtra("video_data");
         videoList = (ArrayList<VideoModel>) getIntent().getSerializableExtra("list_ep");
-        Log.d("DEBUG", "videoList size = " + (videoList == null ? "null" : videoList.size()));
+        Log.d("DEBUGVIDEO", "videoList size = " + (videoList == null ? "null" : videoList.size()));
 
         exoPlayer = new ExoPlayer.Builder(this).build();
         binding.thumbnail.setPlayer(exoPlayer);
@@ -82,6 +107,7 @@ public class WatchVideoActivity extends AppCompatActivity {
 
                         exoPlayer.setMediaSource(mediaSource);
                         exoPlayer.prepare();
+                        exoPlayer.setPlayWhenReady(true);
                         exoPlayer.play();
 
                         new CustomMedia3Controller(WatchVideoActivity.this, binding.thumbnail, episode.getName(), videoList, () -> finish(), new EpisodeAdapter.OnItemClickListener() {
